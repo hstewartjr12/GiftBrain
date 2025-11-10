9b7488ca708a4f3069327b807d7c0643d317c05c
Henry <hstew21@icloud.com>
2025-11-09 20:33:24 -0500

    Initial Commit

---
+++ GiftBrainTests/GiftBrainTests.swift
@@ -0,0 +1,16 @@
+//
+//  GiftBrainTests.swift
+//  GiftBrainTests
+//
+//  Created by Henry Stewart on 11/9/25.
+//
+
+import Testing
+
+struct GiftBrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
